d404a1f7bd1c5bf391e0b2e5ec7077e5a09fd8fa
dlsdk0601 <dlsdk0601@gmail.com>
2023-01-31 00:06:25 +0900

    feat: var, let, const 스코프의 영향 / const의 재할당

--- js/var.js
+++ js/var.js
@@ -33,3 +33,31 @@ if(global === "global"){
 }
 
 console.log(global); // "local"
+// global 이 오염됐다.
+// 이는 var는 함수 스코프 이기 때문에, if 스코프도 전역에 영향을 끼친다.
+
+let global = "global";
+
+if(global === "global"){
+  let global = "local";
+
+  console.log(global); //  "local"
+}
+
+console.log(global); // "global"
+// let 은 블록 스코프안에서만 영향을 받기에 안전하다.
+
+// const > let 을 선호하자.
+const person = {
+  name: "name",
+  age: 20
+}
+
+// person = {} // 값 자체를 바꾸기에 에러가 난다.
+
+person.name = "change";
+person.age = 30;
+// 객체의 내부의 값만 바꾼것이기에 동작 가능하다.
+
+// *** const 는 재할당만 금지된다.
+// 객체, 배열 같은 레퍼런스 객체들을 조작 할 때는 이상이 없다.

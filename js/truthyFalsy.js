6ee8181e1d3e98a4c6f673f3efab4e33b02b497c
dlsdk0601 <dlsdk0601@gmail.com>
2023-02-07 23:31:48 +0900

    feat: truthy falsy 예제 생성

---
+++ js/truthyFalsy.js
@@ -0,0 +1,20 @@
+// *** truthy falsy
+
+// if("string".length > 0){
+if("string".length){
+  //
+}
+
+// if(!isNaN(10)){
+if(10){
+  //
+}
+
+// if(boolean === true){
+if(boolean){
+  //
+}
+
+/*
+* 위의 세가지는 주석에 있는게 정석코드이지만, 그 아래의 조건으로도 동작한다. 왜냐하면 각 값들이 truthy 한 값이기 때문*/
+
